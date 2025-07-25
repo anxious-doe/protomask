4681009613d8b937258e920c6b7fbb6e307f610c
anxious-doe <anxietydoe@pm.me>
2025-07-25 22:26:50 +0100

    create runner function for external use

--- protomask.py
+++ protomask.py
@@ -11,8 +11,6 @@ from anim_exclamation import anim_exclamation
 from anim_chevron_side import anim_chevron_side, anim_chevron_side_blink, anim_chevron_side_to_up
 from anim_chevron_up import anim_chevron_up, anim_chevron_up_blink, anim_chevron_up_to_side
 
-I2C = PimoroniI2C(sda=20, scl=21)
-
 def get_mux_matrix(i2c, channel, address):
     """
     Create a matrix object behind the multiplexer.
@@ -102,45 +100,67 @@ def play_anim(i2c, eye_positions: dict, eye: str, anim: list, framesize: tuple, 
         
         time.sleep(delay)
 
-print("setting up matrices")
-MATRIX0R = get_mux_matrix(I2C, 0, 0x75)
-MATRIX0L = get_mux_matrix(I2C, 0, 0x77)
+def anim_runner(
+        custom_eye_config: dict = None,
+    ):
 
-MATRIX1R = get_mux_matrix(I2C, 1, 0x75)
-MATRIX1L = get_mux_matrix(I2C, 1, 0x77)
+    print(f"setting up I2C")
+    I2C = PimoroniI2C(sda=20, scl=21)
 
-print("initialising variables")
+    print("setting up matrices")
+    MATRIX0R = get_mux_matrix(I2C, 0, 0x75)
+    MATRIX0L = get_mux_matrix(I2C, 0, 0x77)
 
-MATRICES = [MATRIX0L, MATRIX0R, MATRIX1L, MATRIX1R]
+    MATRIX1R = get_mux_matrix(I2C, 1, 0x75)
+    MATRIX1L = get_mux_matrix(I2C, 1, 0x77)
 
-ANIM_DELAY = 0.4
-FRAMESIZE = (11, 14)
-BRIGHTNESS = 60
+    print("initialising variables")
 
-EYE_POSITIONS = {
-    "left_channel": 1,
-    "right_channel": 0,
-    "left_invert_y": False,
-    "right_invert_y": True,
-    "left_left_pos": 0,
-    "left_right_pos": 1,
-    "right_left_pos": 2,
-    "right_right_pos": 3,
-}
+    MATRICES = [MATRIX0L, MATRIX0R, MATRIX1L, MATRIX1R]
 
-print("running")
+    delay = 0.8
+    brightness = 100
+    framesize = (11, 14)
 
-while True:
-    inpt = input(">>>")
-    play_anim(i2c=I2C, eye_positions=EYE_POSITIONS, eye="right", anim=anim_chevron_up, framesize=FRAMESIZE, matrices=MATRICES, brightness=100, delay=0.08)
-    
-    if inpt == "blink":
-        print("blinking")
-        play_anim(i2c=I2C, eye_positions=EYE_POSITIONS, eye="both", anim=anim_chevron_up_blink, framesize=FRAMESIZE, matrices=MATRICES, brightness=100, delay=0.08)
-    elif inpt == "wink":
-        play_anim(i2c=I2C, eye_positions=EYE_POSITIONS, eye="left", anim=anim_chevron_up_blink, framesize=FRAMESIZE, matrices=MATRICES, brightness=100, delay=0.08)
-    elif inpt == "quit":
-        print("quitting")
-        sys.exit()
+    if custom_eye_config is not None:
+        eye_config = custom_eye_config
     else:
-        print(f"not a command: {inpt}")
+        eye_config = {
+            "left_channel": 1,
+            "right_channel": 0,
+            "left_invert_y": False,
+            "right_invert_y": True,
+            "left_left_pos": 0,
+            "left_right_pos": 1,
+            "right_left_pos": 2,
+            "right_right_pos": 3,
+        }
+
+    print("running")
+
+    while True:
+        inpt = input(">>>")
+        play_anim(i2c=I2C, eye_positions=eye_config, eye="right", anim=anim_chevron_up, framesize=framesize, matrices=MATRICES, brightness=brightness, delay=delay)
+        
+        if inpt == "blink":
+            print("blinking")
+            play_anim(i2c=I2C, eye_positions=eye_config, eye="both", anim=anim_chevron_up_blink, framesize=framesize, matrices=MATRICES, brightness=brightness, delay=delay)
+        elif inpt == "wink":
+            play_anim(i2c=I2C, eye_positions=eye_config, eye="left", anim=anim_chevron_up_blink, framesize=framesize, matrices=MATRICES, brightness=brightness, delay=delay)
+        elif inpt == "set delay":
+            value = input("value:")
+            delay = float(value)
+        elif inpt == "set brightness":
+            value = input("value:")
+            brightness = int(value)
+        elif inpt == "quit":
+            print("quitting")
+            sys.exit()
+        else:
+            print(f"not a command: {inpt}")
+
+
+
+
+
+
